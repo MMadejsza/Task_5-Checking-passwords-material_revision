7ed4bad324da64c569108834268c27e518d60073
Maciej 'Matt' Madejsza <maciej.madejsza@gmail.com>
2021-02-16 19:11:39 +0000

    Funcionality done

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const input = document.querySelector("input");
 const passwordsList = ["oNe", "Julia", "twO", "three"]
 const notifications = ["Well Done!", "Come In!", "Correct", "Good User!"]
+const div = document.querySelector(".messageArea");
 
 function check(e) {
-    e.target.value
+    let putPassword = e.target.value;
+    passwordsList.forEach(password => {
+        password.toLowerCase() == putPassword.toLowerCase() ? printNotification() : "";
+    })
 
 }
+function printNotification() {
+    randomNotification = Math.floor(Math.random() * notifications.length)
+    div.textContent = `${notifications[randomNotification]}`
+    input.value = "";
+}
+
 
 input.addEventListener("input", check);
