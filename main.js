1777440381438327e675363ad431f5b6e2d79efc
Maciej 'Matt' Madejsza <maciej.madejsza@gmail.com>
2021-02-16 17:39:35 +0000

    function template created

---
+++ main.js
@@ -0,0 +1,10 @@
+const input = document.querySelector("input");
+const passwordsList = ["oNe", "Julia", "twO", "three"]
+const notifications = ["Well Done!", "Come In!", "Correct", "Good User!"]
+
+function check(e) {
+    e.target.value
+
+}
+
+input.addEventListener("input", check);
